71a1fe1edb3dada2b44823072ac665b60770c62f
jieun615 <je060105@gmail.com>
2025-02-28 21:14:27 +0900

    계산기 과제 Lv2 완료

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
+import com.example.calculator.Calculator;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        Calculator calc = new Calculator();
+        Scanner a = new Scanner(System.in);
+        long result = 0L;
+
+        while (true) {
+            System.out.print("첫 번째 정수를 입력하세요 : ");
+            int num1 = a.nextInt();
+
+            a.nextLine();
+            System.out.print("사칙 연산 기호를 입력하세요 : ");
+            String operand = a.nextLine();
+
+            System.out.print("두 번째 정수를 입력하세요 : ");
+            int num2 = a.nextInt();
+
+            a.nextLine();
+            result = calc.calculate(num1, operand, num2);
+            calc.addHistory(result);
+            System.out.println("결과: " + result);
+
+            System.out.print("지금까지 계산한 리스트를 확인하시겠습니까? y or n : ");
+            String yn1 = a.nextLine().trim().toLowerCase();
+
+            if(yn1.equals("y")) {
+                System.out.println(calc.getHistory());
+            }
+
+            System.out.print("기록을 삭제하시겠습니까? y or n : ");
+            String yn2 = a.nextLine().trim().toLowerCase();
+
+            if(yn2.equals("y")) {
+                calc.removeResult();
+                System.out.println("삭제 되었습니다.");
+            }
+
+            System.out.println("더 계산하시겠습니까? y or exit : ");
+            String yn = a.nextLine().trim().toLowerCase();
+
+            if(yn.equals("exit")) {
+                System.out.println("계산기를 종료합니다.");
+                break;
+            }
+        }
+        a.close();
     }
 }
--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -1,67 +1,61 @@
 package com.example.calculator;
 
-import java.util.Scanner;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Calculator {
-    public static void main(String[] args) {
-        long result;
-        String err;
-        Scanner a = new Scanner(System.in);
-        while (true) {
-            result = 0L;
-            err = "";
-            System.out.println("첫 번째 정수를 입력하세요 : ");
-            int num1 = a.nextInt();
 
-            a.nextLine();
-            System.out.println("사칙 연산 기호를 입력하세요 : ");
-            String operand = a.nextLine();
+    private List<Long> history;
 
-            System.out.println("두 번째 정수를 입력하세요 : ");
-            int num2 = a.nextInt();
+    public Calculator() {
+        this.history = new ArrayList<>();
+    }
+    public long calculate(long num1, String operand, long num2) {
+        long result = 0;
 
-            switch (operand) {
-                case "+":
-                    result = num1 + num2;
-                    break;
-                case "-":
-                    result = num1 - num2;
-                    break;
-                case "*":
-                    result = num1 * num2;
-                    break;
-                case "/":
-                    if (num2 == 0) {
-                        err = "0으로 나눌 수 없습니다.";
-                    } else {
-                        result = num1 / num2;
-                    }
-                    break;
-                case "%":
-                    if (num2 == 0) {
-                        err = "0으로 나눌 수 없습니다.";
-                    } else {
-                        result = num1 % num2;
-                    }
-                    break;
-                default:
-                    err = "잘못된 연산 기호입니다.";
-                    break;
-            }
-            if (!err.isEmpty()) {
-                System.out.println(err);
-            } else {
-                System.out.println("결과: " + result);
-            }
-            a.nextLine();
-            System.out.println("더 계산하시겠습니까? y or exit");
-            String yn = a.nextLine().trim().toLowerCase();
-
-            if(yn.equals("exit")) {
-                System.out.println("계산기를 종료합니다.");
+        switch (operand) {
+            case "+":
+                result = num1 + num2;
                 break;
-            }
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 == 0) {
+                    System.out.println("0으로 나눌 수 없습니다.");
+                    return result;
+                }
+                result = num1 / num2;
+                break;
+            case "%":
+                if (num2 == 0) {
+                    System.out.println("0으로 나눌 수 없습니다.");
+                    return result;
+                }
+                result = num1 % num2;
+                break;
+            default:
+                System.out.println("잘못된 연산 기호입니다.");
+                break;
         }
-        a.close();
+        return result;
+    }
+    public List<Long> getHistory() {
+        return history;
+    }
+
+    public void addHistory(long result) {
+        history.add(result);
+    }
+
+    public void removeResult() {
+        if (!history.isEmpty()) {
+            long removedValue = history.remove(0);
+            System.out.println("삭제된 결과: " + removedValue);
+        } else {
+            System.out.println("저장된 연산 결과가 없습니다.");
+        }
     }
 }
